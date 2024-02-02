a41d15aa740641f8defad8811e708e58de3849da
King Grey <kinggrey9959@gmail.com>
2024-02-02 10:56:27 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4CAF4F",
+        secondary: "#4D4D4D",
+        cement: "#18191F",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif", "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
